d606b0ca93317390073e2b14ce9806b490b35b8f
Steffen Vogel <post@steffenvogel.de>
2023-12-05 01:09:08 +0100

    New Mockfile format including card metadata and timings
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- test/mock.go
+++ test/mock.go
@@ -5,10 +5,13 @@ package test
 
 import (
 	"bufio"
+	"cmp"
 	"encoding/hex"
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -21,9 +24,10 @@ import (
 var _ iso.PCSCCard = (*MockCard)(nil)
 
 type call struct {
-	Method   string
-	Command  []byte
-	Response []byte
+	Start, End time.Time
+	Method     string
+	Command    []byte
+	Response   []byte
 }
 
 // MockCard is a wrapper around iso.PCSCCard which
@@ -78,9 +82,13 @@ func (c *MockCard) Close() error {
 
 func (c *MockCard) Transmit(cmd []byte) (resp []byte, err error) {
 	if c.next != nil {
+		start := time.Now()
+
 		resp, err = c.next.Transmit(cmd)
 
 		c.calls = append(c.calls, call{
+			Start:    start,
+			End:      time.Now(),
 			Method:   "Transmit",
 			Command:  cmd,
 			Response: resp,
@@ -97,11 +105,17 @@ func (c *MockCard) Transmit(cmd []byte) (resp []byte, err error) {
 
 func (c *MockCard) BeginTransaction() error {
 	if c.next != nil {
+		start := time.Now()
+
+		err := c.next.BeginTransaction()
+
 		c.calls = append(c.calls, call{
+			Start:  start,
+			End:    time.Now(),
 			Method: "BeginTransaction",
 		})
 
-		return c.next.BeginTransaction()
+		return err
 	}
 
 	args := c.Mock.MethodCalled("BeginTransaction")
@@ -110,11 +124,17 @@ func (c *MockCard) BeginTransaction() error {
 
 func (c *MockCard) EndTransaction() error {
 	if c.next != nil {
+		start := time.Now()
+
+		err := c.next.EndTransaction()
+
 		c.calls = append(c.calls, call{
+			Start:  start,
+			End:    time.Now(),
 			Method: "EndTransaction",
 		})
 
-		return c.next.EndTransaction()
+		return err
 	}
 
 	args := c.Mock.MethodCalled("EndTransaction")
@@ -139,19 +159,31 @@ func (c *MockCard) LoadTranscript() error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		parts := strings.Split(line, " ")
+		cols := splitColumns(line)
 
-		if len(parts) < 1 || strings.HasPrefix(parts[0], "#") {
+		if len(cols) < 1 || strings.HasPrefix(cols[0], "#") {
 			continue
 		}
 
-		action := parts[0]
+		action := cols[0]
 		if action != "on" {
 			continue
 		}
 
-		method := parts[1]
-		args := parts[2:]
+		start, err := strconv.ParseFloat(cols[1], 64)
+		if err != nil {
+			return fmt.Errorf("failed to parse start column: %w", err)
+		}
+
+		end, err := strconv.ParseFloat(cols[2], 64)
+		if err != nil {
+			return fmt.Errorf("failed to parse end column: %w", err)
+		}
+
+		method := cols[3]
+		args := cols[4:]
+
+		var call *mock.Call
 
 		switch method {
 		case "Transmit":
@@ -165,11 +197,17 @@ func (c *MockCard) LoadTranscript() error {
 				return fmt.Errorf("failed to decode response: %w", err)
 			}
 
-			c.Mock.On(method, cmd).Return(resp, nil).Once()
+			c.Mock.On(method, cmd).
+				Return(resp, nil)
 
 		case "BeginTransaction", "EndTransaction":
-			c.Mock.On(method).Return(nil)
+			c.Mock.On(method).
+				Return(nil)
 		}
+
+		call.
+			Once().
+			After(time.Duration((end - start) * float64(time.Millisecond)))
 	}
 
 	if err := f.Close(); err != nil {
@@ -191,22 +229,71 @@ func (c *MockCard) WriteTranscript() error {
 	if err != nil {
 		return fmt.Errorf("failed to open transcript: %w", err)
 	}
+	defer f.Close()
 
-	fmt.Fprintf(f, "# Mockfile/v1 created=%s\n", time.Now().Format(time.RFC3339))
+	fmt.Fprintln(f, "# Mockfile")
+	fmt.Fprintln(f, "file.version", "v2")
+	fmt.Fprintln(f, "file.created", time.Now().Format(time.RFC3339))
 
+	if hostname, err := os.Hostname(); err == nil {
+		fmt.Fprintln(f, "file.creator", fmt.Sprintf("%s@%s", os.Getenv("USER"), hostname))
+	}
+
+	if mc, ok := c.next.(iso.MetadataCard); ok {
+		forEachSorted(mc.Metadata(), func(key, value string) {
+			fmt.Fprintln(f, "meta", key, value)
+		})
+	}
+
+	if len(c.calls) == 0 {
+		return nil
+	}
+
+	fmt.Fprintf(f, "\n#  %s %s method\n", fmt.Sprintf("%8s", "start"), fmt.Sprintf("%8s", "end"))
+
+	start := c.calls[0].Start
 	for _, call := range c.calls {
+		callStart := fmt.Sprintf("%8.3f", inMilliseconds(call.Start.Sub(start)))
+		callEnd := fmt.Sprintf("%8.3f", inMilliseconds(call.Start.Sub(start)))
+
 		switch call.Method {
 		case "Transmit":
-			fmt.Fprintln(f, "on", "Transmit", hex.EncodeToString(call.Command), hex.EncodeToString(call.Response))
+			fmt.Fprintln(f, "on", callStart, callEnd, call.Method, hex.EncodeToString(call.Command), hex.EncodeToString(call.Response))
 
 		default:
-			fmt.Fprintln(f, "on", call.Method)
+			fmt.Fprintln(f, "on", callStart, callEnd, call.Method)
 		}
 	}
 
-	if err := f.Close(); err != nil {
-		return fmt.Errorf("failed to close transcript: %w", err)
+	return nil
+}
+
+func inMilliseconds(d time.Duration) float64 {
+	return float64(d) / float64(time.Millisecond)
+}
+
+func splitColumns(line string) (cols []string) {
+	r := strings.NewReader(line)
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanWords)
+
+	for s.Scan() {
+		cols = append(cols, s.Text())
 	}
 
-	return nil
+	return cols
+}
+
+func forEachSorted[V any, K cmp.Ordered](s map[K]V, cb func(k K, v V)) {
+	ks := []K{}
+
+	for k := range s {
+		ks = append(ks, k)
+	}
+
+	slices.Sort(ks)
+
+	for _, k := range ks {
+		cb(k, s[k])
+	}
 }
