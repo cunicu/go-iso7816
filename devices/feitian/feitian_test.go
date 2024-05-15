bd9f892b32bfa6735a06bc6525cabfe73ccdc0a3
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-05-15 18:33:14 +0200

    device: Add basic support for FEITIAN tokens
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

---
+++ devices/feitian/feitian_test.go
@@ -0,0 +1,42 @@
+package feitian_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"cunicu.li/go-iso7816"
+	"cunicu.li/go-iso7816/devices/feitian"
+	"cunicu.li/go-iso7816/filter"
+	"cunicu.li/go-iso7816/test"
+)
+
+func withCard(t *testing.T, cb func(t *testing.T, c *feitian.Card)) {
+	test.WithCard(t, filter.IsFeitian, func(t *testing.T, c *iso7816.Card) {
+		cb(t, &feitian.Card{c})
+	})
+}
+
+func TestSerialNumber(t *testing.T) {
+	withCard(t, func(t *testing.T, c *feitian.Card) {
+		require := require.New(t)
+
+		sno, err := c.SerialNumber()
+		require.NoError(err)
+		require.NotEmpty(sno)
+
+		t.Logf("Serial Number: %s", sno)
+	})
+}
+
+func TestCosVersion(t *testing.T) {
+	withCard(t, func(t *testing.T, c *feitian.Card) {
+		require := require.New(t)
+
+		v, err := c.COSVersion()
+		require.NoError(err)
+		require.NotEmpty(v)
+
+		t.Logf("COS version: %s", v)
+	})
+}
